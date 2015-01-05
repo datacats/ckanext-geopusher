5c1b97638f7f6646af5b4c7b9b8218201fdc8100
Denis Zgonjanin <deniszgonjanin@gmail.com>
2015-01-05 18:09:59 -0500

    we can't use ckan-service-provider. it's too tightly coupled with ckan, which needs to set the job type in request headers. We don't want to write a geopusher specific extension

--- geopusher/jobs.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-import ckanserviceprovider.job as job
-import ckanserviceprovider.util as util
-
-@job.async
-def process_resource_notifications(task_id, input, dry_run=False):
-    print 'hello'
