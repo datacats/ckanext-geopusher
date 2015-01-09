e91d7797df991bd5b962b850334bf399589755aa
Denis Zgonjanin <deniszgonjanin@gmail.com>
2015-01-09 14:28:57 -0500

    convert files properly

--- geopusher/app.py
+++ geopusher/app.py
@@ -1,13 +1,21 @@
+import os
 import uuid
 import json
 import shutil
 import ckanapi
+import zipfile
 import logging
 import requests
 import shapefile
 
 from flask import Flask, request
 app = Flask(__name__)
+
+TEMPDIR = 'tmp'
+OUTDIR = os.path.join(TEMPDIR, 'out')
+
+CKAN_URL = os.environ.get(CKAN_URL, None)
+APIKEY = os.environ.get(APIKEY, None)
 
 def convert_file(shapefile_path, outfile_path):
     reader = shapefile.Reader(shapefile_path)
@@ -29,12 +37,21 @@ def convert_file(shapefile_path, outfile_path):
     geojson.close()
 
 def download_file(url):
-    tmpname = 'tmp/{0}.{1}'.format(uuid.uuid1(), 'zip')
+    tmpname = '{0}.{1}'.format(uuid.uuid1(), 'shp.zip')
     response = requests.get(url, stream=True)
-    with open(tmpname, 'wb') as out_file:
+    with open(os.path.join(TEMPDIR, tmpname), 'wb') as out_file:
         shutil.copyfileobj(response.raw, out_file)
 
     return tmpname
+
+def unzip_file(filepath):
+    z = zipfile.ZipFile(os.path.join(TEMPDIR, filepath))
+    dirname = os.path.join(TEMPDIR, filepath[:-4])
+    os.makedirs(dirname)
+    for name in z.namelist():
+        z.extract(name, dirname)
+
+    return dirname
 
 @app.route('/', methods=['POST'])
 def process_webhook():
@@ -45,9 +62,19 @@ def process_webhook():
     if resource.get('format', None) == 'SHP':
         file = download_file(resource['url'])
 
-        outfile = "tmp/out/{0}.{1}".format(resource['name'], 'json')
-        convert_file(file, outfile)
+        unzipped_dir = unzip_file(file)
 
+        shapefile = None
+        for f in os.listdir(unzipped_dir):
+            if f.endswith(".shp"):
+                shapefile = f
+
+        outfile = os.path.join(OUTDIR,
+                              "{0}.{1}".format(resource['name'], 'json'))
+
+        convert_file(os.path.join(unzipped_dir, shapefile), outfile)
+
+        ckan = ckanapi.RemoteCKAN(CKAN_URL, apikey=APIKEY)
         ckan.action.resource_create(
             package_id = resource['package_id'],
             upload = open(outfile),
@@ -55,7 +82,9 @@ def process_webhook():
             name = resource['name']
         )
 
-    return '', 201
+        return '', 201
+
+    return '', 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
